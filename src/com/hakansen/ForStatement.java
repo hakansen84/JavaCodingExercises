421c7ff3b3b51587b441916a5fefa879d27bcf68
hakansen <hakansen84@gmail.com>
2020-05-21 11:51:11 +0300

    first commit

---
+++ src/com/hakansen/ForStatement.java
@@ -0,0 +1,10 @@
+package com.hakansen;
+
+public class ForStatement {
+
+    public static void forStatement() {
+        for(int i=0; i=5; i++){
+            System.out.println("Hello "+i+" times");
+        }
+    }
+}
